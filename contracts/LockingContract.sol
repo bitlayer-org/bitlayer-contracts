ceeb238c61b7860a4d864e28c212c8b28313969e
Leon.Zhang <leon.zhang@tron.network>
2024-01-29 23:50:12 +0800

    chage some words of contract and add the annotations

--- contracts/LockingContract.sol
+++ contracts/LockingContract.sol
@@ -6,102 +6,121 @@ pragma solidity ^0.8.0;
 
 contract LockingContract {
 
+    /**
+     * VestingSchedule is the locking info of beneficiary
+     * @param lockingAmount the locking Amount of beneficiary
+     * @param releasedAmount released Amount of beneficiary,only update when claim 
+     * @param cliffPeriod cliff Period,in this Period, beneficiary can get none token
+     * @param vestingPeriod vesting Period, beneficiary can get LockingToken by Period
+     * @param isActive when VestingSchedule has infomation
+     */
     struct VestingSchedule {
-        uint256 totalStakingAmount;
+        uint256 lockingAmount;
         uint256 releasedAmount;
+        uint256 cliffPeriod;
+        uint256 vestingPeriod;
         bool isActive;
     }
    
-    uint256 public cliffPeriods;
-    uint256 public vestingPeriods;
+    uint256 public periodTime;          // the seconds of every period, both cliff and vesting, eg: 50 
+    address public LockingToken;        // locked token ,erc20 
+    uint256 public startTimestamp;      // init Time ,when constract is build
+    
 
-    uint256 public periodTime;
-    address public StakingToken;
-    uint256 public startTimestamp;
-
-    mapping(address => VestingSchedule) public vestingSchedules;
+    mapping(address => VestingSchedule) public vestingSchedules; // map of beneficiary
 
     event BeneficiaryChanged(address indexed oldBeneficiary, address indexed newBeneficiary);
     event TokensReleased(address indexed beneficiary, uint256 amount);
 
     constructor(
-        address[] memory beneficiaries,
-        uint256[] memory totalStakingAmount,
-        uint256 _cliffPeriods,
-        uint256 _vestingPeriods,
-        uint256 _periodTime,
-        address _stakingToken
+        address[] memory beneficiaries, // array of beneficiaries         
+        uint256[] memory lockingAmounts,// array of lockingAmount one by one beneficiaries    
+        uint256[] memory cliffPeriods,  // array of cliffPeriod one by one beneficiaries    
+        uint256[] memory vestingPeriods, // array of vestingPeriod one by one beneficiaries    
+        uint256 _periodTime, //public periodTime
+        address _lockingToken // lockedToken
     ) {
         require(
-            beneficiaries.length == totalStakingAmount.length,
+            beneficiaries.length == lockingAmounts.length &&
+            beneficiaries.length == cliffPeriods.length &&
+            beneficiaries.length == vestingPeriods.length,
             "Invalid input length"
         );
 
+        uint256 lockingAmountSum;
+        
         for (uint256 i = 0; i < beneficiaries.length; i++) {
-            require(totalStakingAmount[i] > 0, "Total tokens must be greater than zero");
+            require(lockingAmounts[i] > 0, "Total tokens must be greater than zero");
 
             vestingSchedules[beneficiaries[i]] = VestingSchedule(
-                totalStakingAmount[i],
+                lockingAmounts[i],
                 0,
+                cliffPeriods[i],
+                vestingPeriods[i],
                 true
             );
+            lockingAmountSum += lockingAmounts[i];
         }
-        cliffPeriods = _cliffPeriods;
-        vestingPeriods = _vestingPeriods;
+        // TODO this option cannot pass test,need to rewrite
+        // require(lockingAmountSum == IERC20(LockingToken).balanceOf(address(this)),"Locking Balance not Match");
+
         periodTime = _periodTime;
-        StakingToken = _stakingToken;
+        LockingToken = _lockingToken;
 
         startTimestamp = block.timestamp;
 
     }
 
-    // change the beneficiary to a new account
+    // change the beneficiary to a new account, old vestingSchedule will be deleted
     function changeBeneficiary(address newBeneficiary) external {
         VestingSchedule storage schedule = vestingSchedules[msg.sender];
         require(schedule.isActive, "No active vesting schedule found");
         require(!vestingSchedules[newBeneficiary].isActive,"NewBeneficiary is Active");
 
         vestingSchedules[newBeneficiary] = schedule;
-        schedule.isActive = false;
         delete vestingSchedules[msg.sender];
 
         emit BeneficiaryChanged(msg.sender, newBeneficiary);
     }
 
+    // get the beneficiary's Vesting Amount of Locking token when  vestingPeriod has pass 
     function getVestingAmount(address beneficiary) public view returns (uint256) {
         VestingSchedule memory schedule = vestingSchedules[beneficiary];
         require(schedule.isActive, "No active vesting schedule found");
 
-        uint256 currentPeriod = getCurrentPeriod();
+        uint256 currentPeriod = getCurrentPeriod(beneficiary);
         uint256 totalVestingAmount = 0;
-        if(currentPeriod <= cliffPeriods){
+        if(currentPeriod <= schedule.cliffPeriod){
             return 0;
         }else{
-            uint256 vestingPeriod = currentPeriod - cliffPeriods;
-            totalVestingAmount = schedule.totalStakingAmount * vestingPeriod /vestingPeriods;
+            uint256 vestingPeriod = currentPeriod - schedule.cliffPeriod;
+            totalVestingAmount = schedule.lockingAmount * vestingPeriod /schedule.vestingPeriod;
         }
 
         return totalVestingAmount;
     }
 
-    function getCurrentPeriod() internal view returns (uint256) {
+    // get the Current Period, ever in cliffPeriod or vestingPeriod
+    function getCurrentPeriod(address beneficiary) internal view returns (uint256) {
         uint256 currentTimestamp = block.timestamp;
         uint256 timePassed = currentTimestamp - startTimestamp;
         uint256 currentPeriod = timePassed / periodTime;
+        VestingSchedule memory schedule = vestingSchedules[beneficiary];
 
-        if (currentPeriod >= (vestingPeriods + cliffPeriods)) {
-            currentPeriod = vestingPeriods + cliffPeriods;
+        if (currentPeriod >= (schedule.vestingPeriod + schedule.cliffPeriod)) {
+            currentPeriod = schedule.vestingPeriod + schedule.cliffPeriod;
         }
 
         return currentPeriod;
     }
 
+    // claim the allready released token, the amount = VestingAmount - releasedAmount
     function claim() external {
         VestingSchedule storage schedule = vestingSchedules[msg.sender];
         require(schedule.isActive, "No active vesting schedule found");
 
-        uint256 currentPeriod = getCurrentPeriod();
-        require(currentPeriod > cliffPeriods, "Cliff period has not ended yet");
+        uint256 currentPeriod = getCurrentPeriod(msg.sender);
+        require(currentPeriod > schedule.cliffPeriod, "Cliff period has not ended yet");
 
         uint256 totalVestingAmount = getVestingAmount(msg.sender);
         uint256 tokensToRelease = totalVestingAmount - schedule.releasedAmount;
@@ -109,11 +128,11 @@ contract LockingContract {
 
         schedule.releasedAmount += tokensToRelease;
 
-        require(tokensToRelease <=  IERC20(StakingToken).balanceOf(address(this)),"Insufficient balance");
+        require(tokensToRelease <=  IERC20(LockingToken).balanceOf(address(this)), "Insufficient balance");
         
-        require(schedule.releasedAmount <= schedule.totalStakingAmount,"Vesting ended");
+        require(schedule.releasedAmount <= schedule.lockingAmount,"Vesting ended");
 
-        IERC20(StakingToken).transfer(msg.sender, tokensToRelease);
+        IERC20(LockingToken).transfer(msg.sender, tokensToRelease);
 
         emit TokensReleased(msg.sender, tokensToRelease);
     }
