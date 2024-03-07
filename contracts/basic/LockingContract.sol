fc8a70fb2056f04395cfb1c46f7d07b79e056477
Leon.Zhang <leon.zhang@tron.network>
2024-03-07 10:55:58 +0800

    fix: add ;

--- contracts/basic/LockingContract.sol
+++ contracts/basic/LockingContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 import "../interfaces/IERC20.sol";
-import "../builtin/library/SafeERC20.sol"
+import "../builtin/library/SafeERC20.sol";
 
 pragma solidity ^0.8.0;
 
