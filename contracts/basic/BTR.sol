770a31e75f7fd575f4035daa1d8ddb528ac9beec
Hurricane <zliang7190@gmail.com>
2024-04-01 21:36:05 +0800

    change ticker to BTR

--- contracts/basic/BTR.sol
+++ contracts/basic/BTR.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-contract BRC is ERC20, ERC20Permit {
+contract BTR is ERC20, ERC20Permit {
     constructor (
         address[] memory accounts,
         uint256[] memory amounts
-    ) ERC20("BRC Token","BRC") ERC20Permit("BRC Token"){
+    ) ERC20("BTR Token","BTR") ERC20Permit("BTR Token"){
         uint256 tokenAmount = 1_000_000_000 ether;
         require(accounts.length == amounts.length,"Length Not Match");
         for(uint256 i = 0 ; i < accounts.length; i++){
